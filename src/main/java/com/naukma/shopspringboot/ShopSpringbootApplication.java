56765ab118b828e69a4a7d812fc60726d1039eb6
Vlad Marchenko <vlad5000191@gmail.com>
2024-05-05 20:52:45 +0300

    Initial commit

---
+++ src/main/java/com/naukma/shopspringboot/ShopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.naukma.shopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naukma/shopspringboot/ShopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naukma.shopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
