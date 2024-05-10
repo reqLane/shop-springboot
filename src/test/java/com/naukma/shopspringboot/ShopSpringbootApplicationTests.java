c21225a6b499d93bae4cd20db51ca0ea2a4434d9
Vlad Marchenko <vlad5000191@gmail.com>
2024-05-10 23:55:01 +0300

    bug fix

--- src/test/java/com/naukma/shopspringboot/ShopSpringbootApplicationTests.java
+++ src/test/java/com/naukma/shopspringboot/ShopSpringbootApplicationTests.java
@@ -1345,10 +1345,10 @@ class ShopSpringbootApplicationTests {
         ord1.getOrderProducts().add(new OrderProduct(1, ord1, fb3, getRandomColor(fb3), getRandomMaterial(fb3)));
 
         Order ord2 = new Order(vlad);
-        ord1.getOrderProducts().add(new OrderProduct(1, ord2, sh4, getRandomColor(sh4), getRandomMaterial(sh4)));
-        ord1.getOrderProducts().add(new OrderProduct(2, ord2, bc2, getRandomColor(bc2), getRandomMaterial(bc2)));
-        ord1.getOrderProducts().add(new OrderProduct(1, ord2, ct3, getRandomColor(ct3), getRandomMaterial(ct3)));
-        ord1.getOrderProducts().add(new OrderProduct(4, ord2, sc4, getRandomColor(sc4), getRandomMaterial(sc4)));
+        ord2.getOrderProducts().add(new OrderProduct(1, ord2, sh4, getRandomColor(sh4), getRandomMaterial(sh4)));
+        ord2.getOrderProducts().add(new OrderProduct(2, ord2, bc2, getRandomColor(bc2), getRandomMaterial(bc2)));
+        ord2.getOrderProducts().add(new OrderProduct(1, ord2, ct3, getRandomColor(ct3), getRandomMaterial(ct3)));
+        ord2.getOrderProducts().add(new OrderProduct(4, ord2, sc4, getRandomColor(sc4), getRandomMaterial(sc4)));
 
         List<Order> orders = new ArrayList<>(List.of(ord1, ord2));
 
