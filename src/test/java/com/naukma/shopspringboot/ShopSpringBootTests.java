1a18f0110eb00fda914c69eab5af89cf5385d076
Vlad Marchenko <vlad5000191@gmail.com>
2024-05-19 16:04:32 +0300

    Deleted temporary needed files

---
+++ src/test/java/com/naukma/shopspringboot/ShopSpringBootTests.java
@@ -0,0 +1,13 @@
+package com.naukma.shopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShopSpringBootTests {
+
+    @Test
+    public void test() {
+
+    }
+}
